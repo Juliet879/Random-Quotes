5ebe9222e6b0d1299511fc95477cd27da4df0c04
JulietK <julietgisemba000@>
2020-08-26 06:21:39 +0300

    added the date

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -8,9 +8,8 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
-    {id:1,name:'Determine your priorities and focus on them',description:'Eileen McDargh'},
-    {id:2,name:'Reality is wrong, dreams are for real',description:'Tupac'},
-    {id:3,name:'What consumes your mind controls your life',description:'Juliet'},
+    new Quote(1,'Determine your priorities and focus on them','Eileen McDargh',new Date(2020,4,10)),
+    new Quote(2,'Reality is wrong, dreams are for real','Tupac',new Date(2018,7,12))
 
   ];
 
